6e77279b4e615dfad0a1c92f0f1eaf0ded6a3c95
yixuan.wang <yixuan.wang@xiaoyanggroup.cn>
2022-11-27 19:55:26 +0800

    make gen dao

--- app/gen/dao.go
+++ app/gen/dao.go
@@ -1,8 +1,32 @@
 package gen
 
-import "fmt"
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+)
 
-func GenerateDao(dns string) (err error) {
-	fmt.Println(dns)
+func GenerateDao(dsn string) (err error) {
+	var db *gorm.DB
+	if db, err = gorm.Open(mysql.Open(dsn)); err != nil {
+		return
+	}
+
+	var tableList []string
+	if tableList, err = db.Migrator().GetTables(); err != nil {
+		return
+	}
+
+	generator := gen.NewGenerator(gen.Config{
+		ModelPkgPath: "dao/entity",
+	})
+
+	generator.UseDB(db)
+
+	for _, tableName := range tableList {
+		generator.GenerateModel(tableName)
+	}
+
+	generator.Execute()
 	return
 }
